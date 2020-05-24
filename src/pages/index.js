bbd37da3e0962fd72eb820ac54d5b65e3ae7c103
thalesleite <thalesaleite@gmail.com>
2020-05-24 21:27:42 +0100

    adding the Coronavirus data to the map

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,28 +2,26 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import L from 'leaflet';
 
+import axios from 'axios';
+
 import Layout from 'components/Layout';
 import Container from 'components/Container';
 import Map from 'components/Map';
 
-import axios from 'axios';
-
-import gatsby_astronaut from 'assets/images/gatsby-astronaut.jpg';
-
 const LOCATION = {
-  lat: 38.9072,
-  lng: -77.0369,
+  lat: 0,
+  lng: 0,
 };
 const CENTER = [LOCATION.lat, LOCATION.lng];
 const DEFAULT_ZOOM = 2;
 
 const IndexPage = () => {
+
   /**
    * mapEffect
    * @description Fires a callback once the page renders
    * @example Here this is and example of being used to zoom in and set a popup on load
    */
-
   async function mapEffect({ leafletElement: map } = {}) {
     let resp;
 
@@ -37,7 +35,6 @@ const IndexPage = () => {
 
     const { data = [] } = resp;
     const hashData = Array.isArray(data) && data.length > 0;
-    console.log('data:', data);
 
     if (!hashData) return;
 
@@ -58,6 +55,53 @@ const IndexPage = () => {
         }
       })
     }
+
+    const countryPointToLayer = (feature = {}, latlng) => {
+      const { properties = {} } = feature;
+      const { country, updated, cases, deaths, recovered } = properties;
+      let updatedFormatted,
+          casesString;
+
+      casesString = `${cases}`;
+
+      if ( cases > 1000000 ) {
+        casesString = `${casesString.slice( 0, -6 )}M+`;
+      } else if ( cases > 1000 ) {
+        casesString = `${casesString.slice( 0, -3 )}K+`;
+      }
+
+      if ( updated ) {
+        updatedFormatted = new Date(updated).toLocaleString();
+      }
+
+      const html = `
+      <span class="icon-marker">
+          <span class="icon-marker-tooltip">
+            <h2>${country}</h2>
+            <ul>
+              <li><strong>Confirmed:</strong> ${cases}</li>
+              <li><strong>Deaths:</strong> ${deaths}</li>
+              <li><strong>Recovered:</strong> ${recovered}</li>
+              <li><strong>Last Update:</strong> ${updatedFormatted}</li>
+            </ul>
+          </span>
+          ${casesString}
+      </span>`;
+
+      return L.marker( latlng, {
+        icon: L.divIcon({
+          className: 'icon',
+          html
+        }),
+        riseOnHover: true
+      });
+    }
+
+    const geoJsonLayers = new L.GeoJSON(geoJson, {
+      pointToLayer: countryPointToLayer
+    });
+
+    geoJsonLayers.addTo(map);
   }
 
   const mapSettings = {
@@ -74,7 +118,7 @@ const IndexPage = () => {
       </Helmet>
 
       <Map {...mapSettings} />
-
+      
       <Container type="content" className="text-center home-start">
         <h2>Still Getting Started?</h2>
         <p>Run the following in your terminal!</p>
