099eef0ae4df517b16c9ede047d4de1a3b5cffe6
thalesleite <thalesaleite@gmail.com>
2020-05-25 15:45:13 +0100

    refactoring layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,14 +7,16 @@ const Header = () => {
   return (
     <header>
       <Container type="content">
-        <p>My Gatsby Site</p>
+        <p>COVID-19 Worldwide Cases</p>
         <ul>
           <li>
             <Link to="/">Home</Link>
           </li>
-          <li>
-            <Link to="/page-2/">Page 2</Link>
-          </li>
+          {
+            // <li>
+            //   <Link to="/page-2/">Page 2</Link>
+            // </li>
+          }
         </ul>
       </Container>
     </header>
